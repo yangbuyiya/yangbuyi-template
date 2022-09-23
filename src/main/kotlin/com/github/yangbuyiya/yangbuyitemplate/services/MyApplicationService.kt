64d7de804514289ed175e2281c4a987b59dcced3
GitHub Action <action@github.com>
2022-09-23 03:25:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/yangbuyiya/yangbuyitemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yangbuyiya/yangbuyitemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yangbuyiya.yangbuyitemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yangbuyiya.yangbuyitemplate.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/yangbuyiya/yangbuyitemplate/MyPluginTest.kt
+++ src/test/kotlin/com/github/yangbuyiya/yangbuyitemplate/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yangbuyiya.yangbuyitemplate
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
